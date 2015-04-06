d61eb6db9a52331426e268afa0cb3bc83a0839e2
jus0807@mail.ru <ichered@yandex-team.ru>
2015-04-06 21:26:58 +0300

    поправил view

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -47,7 +47,6 @@
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            {{ $AdminNavBar->asUl() }}
             <ul class="nav navbar-nav">
                 <li>
                     <a href="#">About</a>
